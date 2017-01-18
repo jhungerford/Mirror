6116a8d552744602d830a70b67068cf9999127e4
John Hungerford <jhungerford@gmail.com>
2017-01-18 07:26:47 -0700

    Refactor app test.

--- mirror/src/App.js
+++ mirror/src/App.js
@@ -1,30 +1,35 @@
 import React, { Component } from 'react';
-import './css/App.css';
+
+import {IntlProvider} from 'react-intl';
 import Calendar from './Calendar';
 import Clock from './Clock';
 import Weather from './Weather';
 
+import './css/App.css';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="row">
-          <div className="column column-4">
-            <Calendar />
-          </div>
-          <div className="column column-4">
-            <Clock />
-          </div>
-          <div className="column column-4">
-            <div className="row">
-              <Weather city="Boulder" days={ 3 } />
+      <IntlProvider locale="en">
+        <div className="App">
+          <div className="row">
+            <div className="column column-4">
+              <Calendar />
             </div>
-            <div className="row">
-              <Weather city="Vail" days={ 1 } />
+            <div className="column column-4">
+              <Clock />
+            </div>
+            <div className="column column-4">
+              <div className="row">
+                <Weather city="Boulder" days={ 3 } />
+              </div>
+              <div className="row">
+                <Weather city="Vail" days={ 1 } />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </IntlProvider>
     );
   }
 }
--- mirror/src/index.js
+++ mirror/src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {IntlProvider} from 'react-intl';
+
 import './css/index.css';
 
-ReactDOM.render(
-    <IntlProvider locale="en">
-      <App />
-    </IntlProvider>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
