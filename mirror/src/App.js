7aba079e014dcb5a4d72dccb0d31782cf879cbcd
John Hungerford <jhungerford@gmail.com>
2017-01-23 06:20:53 -0700

    Dark Sky link.

--- mirror/src/App.js
+++ mirror/src/App.js
@@ -19,6 +19,7 @@ class App extends Component {
         <div className="column column-4">
           <Weather city="Boulder" days={ 3 } />
           <Weather city="Vail" days={ 1 } />
+          <a className="licenseLink" href="https://darksky.net/dev/">Powered by Dark Sky</a>
         </div>
       </div>
     );
