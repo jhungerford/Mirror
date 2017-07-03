0408873b1109aa376305ff0ac0d10f25a60471ef
John Hungerford <jhungerford@gmail.com>
2017-07-03 15:32:18 -0600

    Implement time component.

--- mirror-react/src/Time.js
+++ mirror-react/src/Time.js
@@ -3,9 +3,35 @@ import './css/Time.css';
 
 class Time extends Component {
 
+  static now = () => new Date();
+  
+  static timeFields(date) {
+    return {
+      hour: date.getHours(),
+      minute: date.getMinutes()
+    };
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = Time.timeFields(Time.now());
+  }
+
+  componentDidMount() {
+    const intervalId = setInterval(this.tick, 1000);
+    this.setState({ intervalId: intervalId });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  tick = () => this.setState(Time.timeFields(Time.now()));
+
   render() {
     return (
-      <div className="time">12:59</div>
+      <div className="time">{ this.state.hour }: { this.state.minute }</div>
     );
   }
 }
